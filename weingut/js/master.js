f092a8f3da75674ca46681860a414014851e1447
JustSquiddy <maurex.privat@outlook.de>
2019-01-29 19:23:31 +0100

    Add files via upload

---
+++ weingut/js/master.js
@@ -0,0 +1,22 @@
+let today = new Date();
+
+$('#date').append(today.getFullYear());
+
+
+// icon changer
+
+if (today.getMonth() >= 0 && today.getMonth() <= 3){
+  $('#icon').attr('src', function(){
+    return '../media/freezy.png';
+  });
+}
+else if (today.getMonth() >= 4 && today.getMonth() <= 7) {
+  $('#icon').attr('src', function(){
+    return '../media/redschy.png';
+  });
+}
+else if (today.getMonth() >= 8 && today.getMonth() <= 11) {
+  $('#icon').attr('src', function(){
+    return '../media/rosy.png';
+  });
+}
