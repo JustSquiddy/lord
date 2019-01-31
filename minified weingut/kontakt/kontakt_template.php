825306a3416a5a16777ff1e2e0ff72805bf7e9bd
LordSquiddy <alex.maurer03@gmail.com>
2019-01-31 16:09:01 +0100

    j

---
+++ minified weingut/kontakt/kontakt_template.php
@@ -0,0 +1,39 @@
+<?php
+ $Empfaenger = "your.mail@example.com";
+?>
+
+<html>
+  <head>
+    <title>Einfaches Kontaktformular mit HTML und PHP</title>
+  </head>
+  <body>
+    <?php
+
+    if($_REQUEST['submit']){
+      if(empty($_REQUEST['firstname']) || empty($_REQUEST['email'])
+       || empty($_REQUEST['lastname']))
+      {
+        echo"Bitte gehen Sie <a href=\"javascript:history.back();\">
+        zur&uuml;ck</a> und f&uuml;llen Sie alle Felder aus";
+      }
+      else{
+        $Mailnachricht="Sie haben eine Anfrage über ihr Kontaktformular erhalten:\n";
+        $Mailnachricht .= "Name: ".$_REQUEST['firstname']." ".$_REQUEST['lastname']."\n".
+        				  "E-Mail: ".$_REQUEST['email']."\n".
+        				  "Datum: ".date("d.m.Y H:i")."\n".
+        				  "\n\n".$_REQUEST['message']."\n";
+
+        $Mailbetreff = "Kontakt: ".$_REQUEST['lastname'];
+        mail($Empfaenger, $Mailbetreff, $Mailnachricht, "From: "
+        .$_REQUEST['email']);
+        echo "Wir haben Ihre Anfrage erhalten und werden sie so schnell wie möglich bearbeiten. <br>
+        <a href=\"javascript:history.back();\">Zur&uuml;ck</a>";
+        }
+    }
+    else
+    {
+      echo"Ein Fehler ist aufgetreten. Hier können Sie eine <a href=\"kontakt.html\">Anfrage</a> an uns senden.";
+    }
+    ?>
+  </body>
+</html>
--- weingut/kontakt/kontakt.php
+++ weingut/kontakt/kontakt.php
@@ -1,34 +1,51 @@
 <?php
-$Empfaenger = „lrdsquddy@gmail.com“;
+$Empfaenger = "lrdsquddy@gmail.com";
 ?>
 <html>
 <head>
-<title>Einfaches Kontaktformular mit HTML und PHP</title>
+<meta http-equiv=“refresh“ content=“3; url=./index.html“>
+<title>Kontakt</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+<style media="screen">
+  .parent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  }
+  body, html {
+    height: 100%;
+  }
+</style>
 </head>
-<body>
+<body class="parent">
+
 <?php
-if($_REQUEST[’submit‘]){
-if(empty($_REQUEST[’firstname‘]) || empty($_REQUEST[‚email‘]) || empty($_REQUEST[‚lastname‘]))
+if($_REQUEST['submit']){
+if(empty($_REQUEST['firstname']) || empty($_REQUEST['email']) || empty($_REQUEST['lastname']))
 {
-echo“Bitte gehen Sie <a href=\“javascript:history.back();\“>
-zur&uuml;ck</a> und f&uuml;llen Sie alle Felder aus“;
+echo"Bitte gehen Sie <a href=\"javascript:history.back();\">
+zur&uuml;ck</a> und f&uuml;llen Sie alle Felder aus";
 }
 else{
-$Mailnachricht=“Sie haben eine Anfrage über ihr Kontaktformular erhalten:\n“;
-$Mailnachricht .= „Name: „.$_REQUEST[’lastname‘].“\n“.
-„E-Mail: „.$_REQUEST[‚email‘].“\n“.
-„Datum: „.date(„d.m.Y H:i“).“\n“.
-„\n\n“.$_REQUEST[‚message‘].“\n“;
-$Mailbetreff = „Kontakt: „.$_REQUEST[‚lastname‘];
-mail($Empfaenger, $Mailbetreff, $Mailnachricht, „From: „
-.$_REQUEST[‚email‘]);
-echo „Wir haben Ihre Anfrage erhalten und werden sie so schnell wie möglich bearbeiten. <br>
-<a href=\“javascript:history.back();\“>Zur&uuml;ck</a>“;
+$Mailnachricht="Sie haben eine Anfrage ueber ihr Kontaktformular erhalten:\n";
+$Mailnachricht .= "Name: ".$_REQUEST['firstname']." ".$_REQUEST['lastname']."\n".
+"E-Mail: ".$_REQUEST['email']."\n".
+"Datum: ".date("d.m.Y H:i")."\n".
+"\n\n".$_REQUEST['message']."\n";
+$Mailbetreff = "Kontakt: ".$_REQUEST['firstname']." ".$_REQUEST['lastname'];
+mail($Empfaenger, $Mailbetreff, $Mailnachricht, "From: "
+.$_REQUEST['email']);
+echo "<body style=\"background-color:#82c91e;\">";
+echo "<h1 style=\"text-align:center;\">Das ging gut. <i class=\"fas fa-paper-plane\"></i>";
+echo "<p style=\"text-align:center;\">Ihre Nachricht wurde versendet.</p>";
 }
 }
 else
 {
-echo“Ein Fehler ist aufgetreten. Hier können Sie eine <a href=\“kontakt.html\“>Anfrage</a> an uns senden.“;
+echo "<body style=\"background-color:#C1121C;\">";
+echo "<h1 style=\"text-align:center;\">Hoopla! Da ging was schief. <i class=\"fas fa-exclamation-triangle\"></i>";
+echo "<p style=\"text-align:center;\">Keine Sorge. Es liegt nicht an Ihnen.</p>";
 }
 ?>
 </body>
